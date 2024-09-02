d386f20fa6cd956050492ffc7ac1b56d0445d8e2
takushisato <takuchanpower@gmail.com>
2024-09-02 21:19:16 +0900

    headerの土台を作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h3>ポートフォリオ</h3>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">ホーム</a>
+          </li>
+          <li>
+            <a href="/">GitHub</a>
+          </li>
+          <li>
+            <a href="/">SNS</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

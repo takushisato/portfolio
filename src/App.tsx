54c68079648427fc123eb6d5d3de71d8aa7552a1
takushisato <takuchanpower@gmail.com>
2024-09-09 00:14:29 +0900

    daisyUIでヘッダー作成（ダミー）

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopPage from "./pages/top";
+import "../src/index.css";
 
 function App() {
   return (
--- src/layout/block/Header.tsx
+++ src/layout/block/Header.tsx
@@ -1,27 +1,28 @@
 const Header = () => {
   return (
     <header>
-      <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid navbar-width">
-          <div className="mx-4">
-            <a className="navbar-brand navbar-button" href="/">
-              Home
-            </a>
-          </div>
-          <div className="mx-4"></div>
-          <button
-            className="navbar-toggler navbar-button"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
+      <div className="navbar bg-primary">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-5 w-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path>
+            </svg>
           </button>
         </div>
-      </nav>
+      </div>
     </header>
   );
 };

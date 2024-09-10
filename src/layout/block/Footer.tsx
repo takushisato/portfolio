f39ade4664b04c15566731d9a965c3ecb3d62438
takushisato <takuchanpower@gmail.com>
2024-09-10 15:24:27 +0900

    footer修正

--- src/layout/block/Footer.tsx
+++ src/layout/block/Footer.tsx
@@ -1,22 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="">
-      <nav className="">
-        <div className="">
-          <div className=""></div>
-          <a className="" href="/">
-            GitHub
-          </a>
-          <a className="" href="/">
-            Twitter
-          </a>
-          <a className="" href="/">
-            FaceBook
-          </a>
-          <div className=""></div>
-        </div>
-      </nav>
-      <p className="">© 2024 takushisato portfolio</p>
+    <footer className="bg-primary">
+      <div className="min-h-6"></div>
+      <p className="text-center">© 2024 たくちゃん portfolio</p>
+      <div className="min-h-6"></div>
     </footer>
   );
 };

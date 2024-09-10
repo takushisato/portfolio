86c7e6d2a1a2a3c210a2063ad2ccc22eed4bbe7c
takushisato <takuchanpower@gmail.com>
2024-09-10 14:31:01 +0900

    ヘッダーを修正。

--- src/layout/block/Header.tsx
+++ src/layout/block/Header.tsx
@@ -1,26 +1,51 @@
 const Header = () => {
   return (
     <header>
-      <div className="navbar bg-primary">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
+      <div className="navbar bg-gray-200 border-b-2 border-gray-300">
+        <div className="max-w-screen-xl m-auto container">
+          <div className="flex-1">
+            <a href="/" className="btn btn-ghost normal-case text-xl">
+              port folio
+            </a>
+          </div>
+          <div className="flex-none">
+            <div className="dropdown dropdown-end">
+              <label className="m-1">
+                <button className="btn btn-square btn-ghost">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block w-5 h-5 stroke-current"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                  </svg>
+                </button>
+              </label>
+              <ul className="dropdown-content z-[1] menu p-2 shadow bg-base-200 rounded-box w-52">
+                <li className="font-bold my-2">
+                  <a href="/">自己紹介</a>
+                </li>
+                <li className="font-bold my-2">
+                  <a href="/">経歴</a>
+                </li>
+                <li className="font-bold my-2">
+                  <a href="/">スキル</a>
+                </li>
+                <li className="font-bold my-2">
+                  <a href="/">個人開発</a>
+                </li>
+                <li className="font-bold my-2">
+                  <a href="/">更新情報</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </header>

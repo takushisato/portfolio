0c36d54d04452bc8d99474d62d6d36b40ffd682e
takushisato <takuchanpower@gmail.com>
2024-09-10 23:09:02 +0900

    更新履歴のマージン調整

--- src/components/site-history/SiteHistoryComponent.tsx
+++ src/components/site-history/SiteHistoryComponent.tsx
@@ -1,6 +1,6 @@
 function SiteHistoryComponent() {
   return (
-    <div className="max-w-screen-md mx-auto text-center">
+    <div className="max-w-screen-md mx-auto text-center my-16">
       <h2 className="text-2xl">更新履歴</h2>
       <p>公開に向けて準備中</p>
     </div>

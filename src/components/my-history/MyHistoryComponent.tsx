0a0881bf3441101f3e50e00cb0295b2a951e5a1d
takushisato <takuchanpower@gmail.com>
2024-09-10 17:48:01 +0900

    経歴を入力

--- src/components/my-history/MyHistoryComponent.tsx
+++ src/components/my-history/MyHistoryComponent.tsx
@@ -1,59 +1,72 @@
 function MyHistoryCOmponent() {
   const myHistory = [
     {
+      id: 1,
       date: "1982年8月",
       content: "岩手県生まれ",
     },
     {
-      date: "2001年3月",
-      content: "岩手県立一関工業高校 電気科卒業",
-    },
-    {
+      id: 2,
       date: "2001年4月",
-      content: "トステム株式会社 入社",
+      content: "トステム株式会社（現在のリクシル株式会社） 入社",
     },
     {
+      id: 3,
       date: "2004年1月",
       content: "トステム株式会社を退社",
     },
     {
+      id: 4,
       date: "2004年2月",
       content: "中小の金属加工会社に入社",
     },
     {
+      id: 5,
       date: "2019年1月",
       content: "プログラミング学習開始",
     },
     {
+      id: 6,
       date: "2019年10月",
       content: "同社を退社。また、個人でも仕事をするため屋号を取得",
     },
     {
+      id: 7,
       date: "2020年2月頃",
-      content:
-        "コロナが世界的に蔓延。リモートワークが注目され、ITエンジニア志望者が急増し業界に新人が飽和する。そして仕事が取りにくくなったため知り合いの工場でバイトなどを行いながらプログラミングの学習を続ける",
+      content: "コロナが世界的に蔓延する中、プログラミングの学習を続ける",
     },
     {
+      id: 8,
       date: "2021年10月頃",
       content: "IT系の勉強会で知り合ったIT社長の元で修行を始める",
     },
     {
+      id: 9,
       date: "2022年4月",
-      content: "IT系のフリーランスとして完全に独立",
+      content: "IT系のフリーランスとして独立",
     },
   ];
   return (
-    <div>
-      <h2 className="text-2xl">経歴</h2>
-      <p>私の経歴です</p>
-      <ul>
-        {myHistory.map((history, index) => (
-          <li key={index}>
-            <span>{history.date}</span>
-            <span>{history.content}</span>
-          </li>
-        ))}
-      </ul>
+    <div className="my-16 max-w-screen-md mx-auto">
+      <h2 className="text-2xl text-center">経歴</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          <thead>
+            <tr>
+              <th>日付</th>
+              <th>概要</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myHistory.map((history, index) => (
+              <tr key={index}>
+                <td>{history.date}</td>
+                <td>{history.content}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }

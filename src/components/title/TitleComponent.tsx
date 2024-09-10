0db4bc4dd0c6ae1d164a8b3dfdccfdcc1bb53801
takushisato <takuchanpower@gmail.com>
2024-09-10 16:38:48 +0900

    タイトル回りをざっくり作成

--- src/components/title/TitleComponent.tsx
+++ src/components/title/TitleComponent.tsx
@@ -1,8 +1,8 @@
 function TitleComponent() {
   return (
-    <div className="text-2xl my-16">
+    <div className="text-2xl my-16 text-center">
       <h2>ようこそ！</h2>
-      <p>Takushi Satoのポートフォリオへ！</p>
+      <p className="mt-2 text-lg">「たくちゃん」のポートフォリオへ！</p>
     </div>
   );
 }

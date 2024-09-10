71c210d938d985df25fd7c4277703252890c3f1d
takushisato <takuchanpower@gmail.com>
2024-09-10 22:45:12 +0900

    更新履歴を修正

--- src/components/site-history/SiteHistoryComponent.tsx
+++ src/components/site-history/SiteHistoryComponent.tsx
@@ -2,7 +2,7 @@ function SiteHistoryComponent() {
   return (
     <div className="max-w-screen-md mx-auto text-center">
       <h2 className="text-2xl">更新履歴</h2>
-      <p>更新履歴です</p>
+      <p>公開に向けて準備中</p>
     </div>
   );
 }

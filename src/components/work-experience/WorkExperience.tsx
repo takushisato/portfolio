07ce1cff463f6349796e178e1bdf216ad5dc23ef
takushisato <takuchanpower@gmail.com>
2024-09-13 23:09:17 +0900

    WorkExperienceに状態管理したモーダルを追加

--- src/components/work-experience/WorkExperience.tsx
+++ src/components/work-experience/WorkExperience.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 function PersonalDevComponent() {
   const personalDevList = [
     {
@@ -34,6 +35,19 @@ function PersonalDevComponent() {
     },
   ];
 
+  // historyIDをuseStateで管理
+  const [workExperienceID, setWorkExperienceID] = useState<number | null>(null);
+
+  // 行をクリックしたときにモーダルを表示
+  const handleRowClick = (id: number) => {
+    const modal = document.getElementById("my_modal_2");
+    if (modal instanceof HTMLDialogElement) {
+      modal.showModal();
+      setWorkExperienceID(id);
+      console.log("id", id);
+    }
+  };
+
   return (
     <div className="text-center my-16 max-w-screen-xl mx-auto">
       <h2 className="text-2xl">実務経歴</h2>
@@ -44,7 +58,8 @@ function PersonalDevComponent() {
         {personalDevList.map((item) => (
           <div
             key={item.id}
-            className="w-full md:w-1/3 lg:w-1/5 bg-slate-300 p-4 rounded-md m-4"
+            className="w-full md:w-1/3 lg:w-1/5 bg-slate-300 p-4 rounded-md m-4 cursor-pointer"
+            onClick={() => handleRowClick(item.id)}
           >
             <h3>{item.title}</h3>
             <p className="font-bold">使用技術</p>
@@ -56,6 +71,21 @@ function PersonalDevComponent() {
           </div>
         ))}
       </div>
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">詳細情報</h3>
+          {workExperienceID !== null && (
+            <p className="py-4">
+              {personalDevList[workExperienceID - 1].description}
+            </p>
+          )}
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 }

a7bdf2cd1d4448a8ce226be6a4862556417d9e59
takushisato <takuchanpower@gmail.com>
2024-09-28 23:41:12 +0900

    更新履歴を修正

--- src/components/site-history/SiteHistoryComponent.tsx
+++ src/components/site-history/SiteHistoryComponent.tsx
@@ -1,8 +1,31 @@
 function SiteHistoryComponent() {
   return (
-    <div className="max-w-screen-md mx-auto text-center my-16" id="siteHistory">
+    <div className="max-w-screen-sm mx-auto text-center my-16" id="siteHistory">
       <h2 className="text-2xl">更新履歴</h2>
-      <p>公開に向けて準備中</p>
+      <div className="h-24 overflow-x-auto">
+        <table className="table table-pin-rows">
+          <tbody>
+            <tr>
+              <td>
+                <span className="font-bold">2024/9/30</span>{" "}
+                <span className="mx-8"></span>スムーススクロールを追加しました。
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span className="font-bold">2024/9/15</span>{" "}
+                <span className="mx-8"></span> 経歴、個人開発を修正しました。
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span className="font-bold">2024/9/01</span>{" "}
+                <span className="mx-8"></span> 新規公開しました。
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
